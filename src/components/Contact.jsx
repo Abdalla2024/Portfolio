f0bd4e532e9c918d8566f27723719618ba45d76a
Abdalla Abdelmagid <abdallaa2024@gmail.com>
2025-01-28 00:07:32 -0500

    Set up the contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,16 +33,16 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_g1d9qkg',
+        'template_l6fxwjo',
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Abdalla Abdelmagid",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "abdallaa2024@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        'S8Xc0y1M2mRKAiv4G'
       )
       .then(
         () => {
